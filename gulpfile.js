9427fb560ac5411447fcc04eb3531d19c9d83bfe
afeiship <1290657123@qq.com>
2021-08-04 13:18:17 +0800

    feat: initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,13 @@
-/**
- *  Welcome to your gulpfile!
- *  The gulp tasks are splitted in several files in the gulp directory
- *  because putting all here was really too long
- */
+(function() {
+  'use strict';
 
-'use strict';
+  const gulp = require('gulp');
+  const fs = require('fs');
 
-var gulp = require('gulp');
-var del = require('del');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var scripts = [
-  'src/angular/main.js',
-  'src/angular/directive/isloading.js',
-  'src/angular/service/isloading.js'
-];
+  //import
+  fs.readdirSync('./build').map(function(file) {
+    require('./build/' + file);
+  });
 
-
-gulp.task('clean', function () {
-  return del('dist');
-});
-
-
-gulp.task('scripts', function () {
-  return gulp.src(scripts)
-    .pipe(concat('angular-isloading.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".js"
-    }))
-    .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('default', ['clean'], function () {
-  gulp.start(['scripts']);
-});
+  gulp.task('default', gulp.series(['clean', 'scripts']));
+})();
